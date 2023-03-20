ccf6cbc130d9729c66f872613a3935393996d039
gislainmba <gislainmba@yahoo.fr>
2023-03-20 15:18:05 -0400

    some changes

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -26,13 +26,28 @@ def about(request):
 # Create a `contact` view to return a static contact page
 def contact(request):
     return render(request, 'djangoapp/contact.html')
+
 # Create a `login_request` view to handle sign in request
-# def login_request(request):
+def login_request(request):
+    context = {}
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['psw']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('djangoapp:index')
+        else:
+            context['message'] = "Invalid username or password."
+            return render(request, 'djangoapp/user_login_bootstrap.html', context)
+    else:
+        return render(request, 'djangoapp/user_login_bootstrap.html', context)
 # ...
 
 # Create a `logout_request` view to handle sign out request
-# def logout_request(request):
-# ...
+def logout_request(request):
+    logout(request)
+    return redirect('djangoapp:index')
 
 # Create a `registration_request` view to handle sign up request
 # def registration_request(request):
