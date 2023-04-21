a9e1e62bdf38d7f78a6018835a592e5192826831
Gislain <gislainmba@yahoo.fr>
2023-04-21 14:59:13 -0400

    updates urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -19,9 +19,10 @@ urlpatterns = [
     path(route='login/', view=views.login_request, name='login'),
     # path for logout
     path(route='logout/', view=views.logout_request, name='logout'),
-    path(route='', view=views.get_dealerships, name='index'),
     # path for dealer reviews view
     path('dealer/<int:dealer_id>/', views.get_dealer_details, name='dealer_details'),
     # path for add a review view
     path('add_review/<int:dealer_id>/', views.add_review, name='add_review'),
+    # path get_dealerships
+    path(route='', view=views.get_dealerships, name='index'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
