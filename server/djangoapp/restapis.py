f71546428f287eceadc019db63d03da352b2ffdc
Gislain <gislainmba@yahoo.fr>
2023-04-29 15:14:48 -0400

    All done

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -13,16 +13,19 @@ from ibm_watson.natural_language_understanding_v1 import Features, SentimentOpti
 def get_request(url, **kwargs):
     print(kwargs)
     print("GET from {} ".format(url))
+    apikey = kwargs.get("d7tNnp6ImXgbIX-8SIqzVG-AvgUUocL0J9ctwCOlcvMn")
     try:
-        # Call get method of requests library with URL and parameters
-         if apikey:
+        if apikey:
             params = dict()
             params["text"] = kwargs["text"]
             params["version"] = kwargs["version"]
             params["features"] = kwargs["features"]
             params["return_analyzed_text"] = kwargs["return_analyzed_text"]
-            response = requests.get(url, headers={'Content-Type': 'application/json'},
-                                    params=kwargs)
+            
+            response = requests.get(url, data=params, auth=HTTPBasicAuth('d7tNnp6ImXgbIX-8SIqzVG-AvgUUocL0J9ctwCOlcvMn', apikey), headers={'Content-Type': 'application/json'},)
+        else:
+            # Call get method of requests library with URL and parameters
+            response = requests.get(url, headers={'Content-Type': 'application/json'}, params=kwargs)
     except:
         # If any error occurs
         print("Network exception occurred")
@@ -56,21 +59,29 @@ def post_request(url, json_payload, **kwargs):
     
 def get_dealers_from_cf(url, **kwargs):
     results = []
-    # Call get_request with a URL parameter
-    json_result = get_request(url)
+    state = kwargs.get("state")
+    if state:
+        json_result = get_request(url, state=state)
+    else:
+        json_result = get_request(url)
+
+    # print('json_result from line 31', json_result)    
+
     if json_result:
         # Get the row list in JSON as dealers
-        dealers = json_result["rows"]
+        dealers = json_result
         # For each dealer object
         for dealer in dealers:
             # Get its content in `doc` object
             dealer_doc = dealer["doc"]
+            # print(dealer_doc)
             # Create a CarDealer object with values in `doc` object
-            dealer_obj = CarDealer(address=dealer_doc["address"], city=dealer_doc["city"], full_name=dealer_doc["full_name"],
-                                   id=dealer_doc["id"], lat=dealer_doc["lat"], long=dealer_doc["long"],
-                                   short_name=dealer_doc["short_name"],
-                                   st=dealer_doc["st"], zip=dealer_doc["zip"])
+            dealer_obj = CarDealer(address=dealer_doc["address"], city=dealer_doc["city"],
+                                   id=dealer_doc["id"], lat=dealer_doc["lat"], long=dealer_doc["long"], full_name=dealer_doc["full_name"],
+                                
+                                   st=dealer_doc["st"], zip=dealer_doc["zip"], short_name=dealer_doc["short_name"])
             results.append(dealer_obj)
+
     return results
 
 
